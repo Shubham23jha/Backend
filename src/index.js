1d71741fbb3773737d5c9b36345b3078437e096c
Shubham23jha <skj23072002@gmail.com>
2023-12-24 13:40:55 +0530

    Registration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
+
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+
 dotenv.config({
   path: "./.env",
 });
@@ -19,28 +21,3 @@ connectDB()
     console.log("MONGO db connection failed !!! ", err);
   });
 
-/*
-import express from "express";
-const app = express();
-
-
-;(async()=>{
-
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-       app.on("error",(err)=>{
-        console.log("Error connecting", err);
-        throw err;
-       });
-
-       app.listen(process.env.PORT,()=>{
-        console.log(`listening on ${process.env.PORT}`);
-       })
-    } catch (error) {
-        console.error("Error :",error);
-        throw error;
-    }
-})()
-
-*/
